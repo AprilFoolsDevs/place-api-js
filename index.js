4fe21b3121ca694f27ec5ce2945a0864c0afc0ce
Oden <ops991923@gmail.com>
2017-03-31 20:33:04 -0700

    Reorganization

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 (function(self) {
 	self.PlaceAPI = {
 		initialized: false,
-		width: 1000,
-		height: 1000,
-		
+		width: 0,
+		height: 0,
 
-		init() {
+
+		init(opts) {
+			// defaults
+			opts = Object.assign({
+				modhash: r.config.modhash
+			}, opts);
+
 			return new Promise((resolve, reject) => {
 				if (this.initialized) resolve();
 
+				this.headers = {
+					"x-modhash": opts.modhash
+				};
 				this.width = r.config.place_canvas_width;
 				this.height = r.config.place_canvas_height;
 				this.initialized = true;
 			});
 		},
 
+		downloadBitmap() {
+			return fetch("https://www.reddit.com/api/place/board-bitmap", {
+					headers: this.headers
+				})
+				.then(res => res.arrayBuffer());
+		},
+
 		parseBitmap(input) {
-			const out = new Uint8Array(this.width * this.height);
 			const buf = new Uint8Array(input.buffer, 4);
+			const out = new Uint8Array(this.width * this.height);
 			for (let i = 0; i < input.byteLength; i++) {
-				out[2 * i] = buf[i] >> 4, out[2 * i + 1] = buf[i] & 15;
+				out[2 * i] = buf[i] >> 4;
+				out[2 * i + 1] = buf[i] & 15;
 			}
 
 			return out;
 		},
 
-		downloadBitmap() {
-			return fetch("https://www.reddit.com/api/place/board-bitmap", {
-					credentials: "include"
-				})
-				.then(res => res.arrayBuffer());
-		},
-		
 		getBitmapArray() {
 			return this.downloadBitmap()
 				.then(buf => PlaceAPI.parseBitmap(new Uint8Array(buf)));
